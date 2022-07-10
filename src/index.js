f94a69e0e0a6c587961e2ac749259207f48eb1cf
Rajan <81413911+RaazanDongol@users.noreply.github.com>
2022-07-10 03:24:21 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+// JS Goes here - ES6 supported
+
+
+ <!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+
+<h1>This is a Heading</h1>
+<p>This is a paragraph.</p>
+
+</body>
+</html> 
